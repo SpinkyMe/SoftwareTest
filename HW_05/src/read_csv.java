330ab27b5a376498f39e31c37c8e9c6ad961f45e
Hasakki23 <1686627320@qq.com>
2022-04-17 12:26:49 +0800

    new

--- HW_05/src/read_csv.java
+++ HW_05/src/read_csv.java
@@ -4,23 +4,26 @@ import java.io.FileReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class read_csv {
     public static final String delimiter = ",";
-    public static ArrayList<String[]> read(String CSVFile){
-        ArrayList<String[]> namelist = new ArrayList<String[]>();
+    public static List<Object[]> read(String CSVFile){
+        List<Object[]> namelist = new ArrayList<>();
         try {
             File file = new File(CSVFile);
             FileReader fileReader = new FileReader(file,StandardCharsets.UTF_8);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = " ";
             String[] tempArr;
+//            String name;
+//            String code;
             while((line = bufferedReader.readLine())!=null){
                 tempArr=line.split(delimiter);
-                for (String string : tempArr) {
-                    System.out.println(string);
-                }
-                namelist.add(tempArr);
+                String name = tempArr[1];
+                String code = tempArr[0];
+                namelist.add(new Object[] {name,code});
             }
 
             fileReader.close();
@@ -28,7 +31,7 @@ public class read_csv {
         } catch (Exception e) {
             e.printStackTrace();
         }
-            String[] namelistArrary = namelist.toArray(new String[0]);
+//            String[] namelistArrary = namelist.toArray(new String[0]);
             return namelist;
     }
 }
